2b9b8c6aed9ef084f0c1d96283573f0b5be55326
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-07-14 21:00:43 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,16 +1,18 @@
 library(shiny)
 library(ggplot2)
 
-fluidPage(
-
+ui <- fluidPage(
+  
   titlePanel("COVID19 excess deaths by LA"),
-
+  
   sidebarPanel(
-
-    selectInput('LA', 'Select LA', sort(data$name)),
-    selectInput('plottype', 'Select plot', c("All deaths"=1,"By cause"=2,"By location"=3,"Cases vs deaths"=4))
+    
+    selectInput('LA', 'Select LA', choices=names),
+    selectInput('plottype', 'Select plot', c("All deaths"=1,"By cause"=2,"By location"=3,
+                                             "Cases vs deaths (Cases E&W only)"=4, "Case numbers (E&W only)"=5, 
+                                             "Cases by pillar (England only)"=6))
   ),
-
+  
   mainPanel(
     plotOutput('plot')
   )
